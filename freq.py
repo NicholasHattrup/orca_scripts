07d95301ffe4da3ff65e20fc71755faeffdfd90c
Nick Hattrup <nickhattrup@Nicks-Mac-PC.local>
2023-03-08 14:08:52 -0500

    freq.py

--- freq.py
+++ freq.py
@@ -1,8 +1,20 @@
-import sys
+from argparse import ArgumentParser 
 import numpy as np
 
 
-xyz_file = sys.argv[1]
+
+parser = ArgumentParser()
+parser.add_argument('--xyz_file', help='xyz file')
+parser.add_argument('--gbw', default=None,help='gbw file')
+parser.add_argument('--charge',default=0, help='charge')
+parser.add_argument('--mult',default=1, help='multiplicity')
+parser.add_argument('--job', help='orca job')
+parser.add_argument('--procs',default=1, help='number of processors')
+
+args = parser.parse_args()
+
+
+xyz_file = args.xyz_file
 
 # Read in xyz file
 with open(xyz_file, 'r') as f:
@@ -19,9 +31,14 @@ for i in range(2, num_atoms + 2):
     positions[i - 2, :] = float(x), float(y), float(z)
 
 # Write orca job
-with open(f'{xyz_file[:-4]}_freq.inp', 'w') as f:
-    f.write('! FREQ ωB97X def2-TZVPPD D4\n')
-    f.write('*xyz 0 1\n')
+with open(f'{args.job}_freq.inp', 'w') as f:
+    if not args.gbw is None:
+        f.write('! FREQ ωB97X def2-TZVPPD D4 MORead\n')
+        f.write(f'%moinp "{args.gbw}"\n')
+    else:
+        f.write('! FREQ ωB97X def2-TZVPPD D4\n')
+    f.write(f'%pal nprocs {args.procs} end\n')
+    f.write(f'*xyz {args.charge} {args.mult}\n')
     for atom, position in zip(atoms, positions):
         f.write(f'{atom} {position[0]} {position[1]} {position[2]} \n')
     f.write('*')
